b3c314d3d4005980370f4eeaa4edfa83e59a6080
ponmanip <ponmani85@gmail.com>
2017-06-21 00:35:15 +0530

    Enabled Springboot Auto Configuration

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,10 @@
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
 /**
  * Created by gyo7 on 27/05/17.
  */
+@EnableAutoConfiguration
 public class App {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(App.class, args);
